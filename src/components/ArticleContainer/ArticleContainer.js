0b46abb9397b2dda95727b2dfa93a3841a9780eb
MatthewPress <press.matt14@gmail.com>
2023-01-11 12:57:31 -0700

    Feat: add initial ArticleContainer component

---
+++ src/components/ArticleContainer/ArticleContainer.js
@@ -0,0 +1,11 @@
+import './ArticleContainer.css';
+
+function ArticleContainer() {
+  return (
+    <section>
+      <p>ArticleContainer</p>
+    </section>
+  )
+}
+
+export default ArticleContainer;
