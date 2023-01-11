3ef955d9502118247e894a14c7e8f4c42b246ab9
MatthewPress <press.matt14@gmail.com>
2023-01-11 12:32:37 -0700

    Feat: add initial api call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react';
+import { getData } from '../../apiCalls/apiCalls';
 import './App.css';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getData('home')
+      .then(data => {
+        setArticles(data.results)
+      })
+  }, []);
+
   return (
    <p>Hey</p>
   );
