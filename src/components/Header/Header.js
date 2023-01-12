77b3fb70935e978e8e560c2a46cd80f7c7b7e6d9
MatthewPress <press.matt14@gmail.com>
2023-01-12 10:24:24 -0700

    Refactor: add header styling and link to landing page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
+
 import './Header.css';
 
 function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <Link to='/' style={{ textDecoration: "none" }}>
+        <h1>Yeet Times</h1>
+      </Link>
+      <div className='line first-line'></div>
+      <div className='line second-line'></div>
     </header>
   )
 }
