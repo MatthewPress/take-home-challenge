d24f92da03ebf464c8ad5150a45131015678f627
MatthewPress <press.matt14@gmail.com>
2023-01-12 10:32:15 -0700

    Test: add initial test

--- cypress/e2e/landing-page-spec.cy.js
+++ cypress/e2e/landing-page-spec.cy.js
@@ -1,5 +1,10 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should display a see a header', () => {
+    cy.get('Header')
+      .should('be.visible')
   })
 })
