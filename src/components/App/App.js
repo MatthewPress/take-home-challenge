b28434d0f4914284005b25fa74be282fb94df359
MatthewPress <press.matt14@gmail.com>
2023-01-12 10:33:52 -0700

    Feat: import dependencies, build out methods, create routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import Header from '../Header/Header';
+import NavBar from '../NavBar/NavBar';
 import ArticleContainer from '../ArticleContainer/ArticleContainer';
 import ArticlePage from '../ArticlePage/ArticlePage';
 
@@ -11,24 +12,71 @@ import './App.css';
 
 function App() {
   const [articles, setArticles] = useState([]);
+  const [filteredArticles, setFilteredArticles] = useState([]);
+  const [storyTypes, setStoryTypes] = useState([]);
   const [selectedArticle, setSelectedArticle] = useState({});
 
   useEffect(() => {
-    getData('home')
+    getData('topstories', 'home', '')
       .then(data => {
-        setArticles(data.results)
+        setArticles(data.results);
+        
+        const subSections = [];
+        data.results?.forEach(result => {
+          if (!subSections.includes(result.subsection) && result.subsection) {
+            subSections.push(result.subsection);
+          }
+        setStoryTypes(subSections);
+        })
       })
   }, []);
+
+  const navigate = useNavigate();
+
+  const filterArticles = (event) => {
+    const selectedTopic = event.target.id;
+
+    if (selectedTopic === 'all') {
+      setFilteredArticles([]);
+      navigate(`/`);
+    } else {
+      const selectedArticles = articles.filter(article => article.subsection === selectedTopic);
+      setFilteredArticles(selectedArticles);
+      navigate(`/${selectedTopic}`);
+    }
+  }
 
   return (
     <>
       <Header />
-      <main>
         <Routes>
-          <Route path='/' element={<ArticleContainer articles={articles} setSelectedArticle={setSelectedArticle} />} />
-          <Route path='/:uri' element={<ArticlePage selectedArticle={selectedArticle} />} />
+          <Route 
+            path='/' 
+            element={
+              <main>
+                <NavBar storyTypes={storyTypes} filterArticles={filterArticles} />
+                <ArticleContainer articles={articles} setSelectedArticle={setSelectedArticle} />
+              </main>
+            } 
+          />
+          <Route
+            path='/:topic'
+            element={
+              <main>
+                <NavBar storyTypes={storyTypes} filterArticles={filterArticles} />
+                <ArticleContainer articles={filteredArticles} setSelectedArticle={setSelectedArticle} />
+              </main>
+            }
+          />
+          <Route 
+            path='/article/:uri' 
+            element={
+              <main>
+                <ArticlePage selectedArticle={selectedArticle} />
+              </main>
+            } 
+          />
         </Routes>
-      </main>
     </>
   );
 }
