9e081aaae233afc8e6868ef41d99a6d0ebabe69d
MatthewPress <press.matt14@gmail.com>
2023-01-12 10:22:25 -0700

    Refactor: make get request more dynamic

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,6 +1,6 @@
-const getData = async (path) => {
+const getData = async (type, path, criteria) => {
   try {
-    const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${path}.json?api-key=${process.env.REACT_APP_NYT_KEY}`)
+    const response = await fetch(`https://api.nytimes.com/svc/${type}/v2/${path}.json?${criteria}api-key=${process.env.REACT_APP_NYT_KEY}`)
     if (!response.ok) {
       throw new Error();
     } else {
